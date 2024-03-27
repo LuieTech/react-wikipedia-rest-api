f733e19ea0ad134edddd2e41f9c945be1a9ac818
LuieR-RMT-WDPT-MAY2023 <rhluisenrique@gmail.com>
2024-03-27 16:48:55 -0400

    chore:"implement HomePage component to display all countries linking each name to its details, hooks useState and useEffect API's call to Api service file"

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,42 @@
-function HomePage() {}
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import countryService from "../../services/country-service";
+
+
+function HomePage() {
+
+  const [countries, setCountries] = useState(null)
+
+  useEffect(() => {
+
+    async function fetchCountries() {
+
+      const countries = await countryService.countries();
+      setCountries(countries)
+
+    }
+
+    fetchCountries()
+
+  }, [])
+
+  if(countries === null) {
+    return <div>Loading...</div>
+  }
+
+  return(
+    
+    <div>
+      {countries.map(c => (
+        
+        // <div key={c._id}><Link to={`/${c.}`}>{c.name.common}</Link></div>
+        // <div key={c._id}><Link to={`/country?id=${c._id}`}>{c.name.common}</Link></div>
+        <div key={c._id}><Link to={`/${c.alpha3Code}`}>{c.name.common}</Link></div>
+        
+      ))}
+    </div>
+  )
+
+}
 
 export default HomePage;
