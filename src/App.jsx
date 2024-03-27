61ec9b7029dd8df4f8193a218da11fb6f97d67ac
LuieR-RMT-WDPT-MAY2023 <rhluisenrique@gmail.com>
2024-03-27 16:46:24 -0400

    chore:"Implement app.js with routes/route, and 2 components "

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import HomePage from "./pages/HomePage";
+import CountryDetails from "./pages/CountryDetailsPage";
 
 function App() {
   return (
     <div className="App">
       <h1>LAB | React WikiCountries</h1>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/:alpha3Code" element={<CountryDetails />} /> {/* => working with Params*/}
+        {/* <Route path="/country" element={<CountryDetails />} /> Working with Query's */}
+      </Routes>
     </div>
   );
 }
