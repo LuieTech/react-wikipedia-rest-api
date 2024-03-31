5f8672260b61ac2164a0438ae93a3fabacfaf0ac
LuieR-RMT-WDPT-MAY2023 <rhluisenrique@gmail.com>
2024-03-31 18:14:09 -0400

    chore:"implement country details component to show array of border countries also"

--- src/pages/CountryDetailsPage.jsx
+++ src/pages/CountryDetailsPage.jsx
@@ -1,12 +1,13 @@
 import { useEffect, useState } from "react";
 import { Link, useLocation, useParams } from "react-router-dom";
 import countryService from "../../services/country-service";
+import "./HomeDetails.css"
 
 function CountryDetails() {
 
   /* Working with Params */
-  const params = useParams() 
-  console.log("Este es el params",params)
+  const {alpha3Code} = useParams() /* useParams is an object { key:value }  */
+  //console.log("Este es el params",params)
 
   /* Working with Query's */
   //const location = useLocation() 
@@ -14,13 +15,13 @@ function CountryDetails() {
   //const query = new URLSearchParams(location.search).get("id") 
 
   const [country, setCountry] = useState(null);
-  console.log("Este es el estado: ", country)
+  console.log("este es el estado: ", country)
 
   useEffect(() => {
     async function fetchCountry() {
 
-      const country = await countryService.details(params.alpha3Code)
-      console.log("Dentro del useEffect",country)
+      const country = await countryService.details(alpha3Code)
+      // console.log("Dentro del useEffect",country)
       setCountry(country)
 
     }
@@ -34,11 +35,52 @@ function CountryDetails() {
 
   return(
     <>
-      <div>Country name : {country.name.common}</div> {/* Working with params */}
+      <div className="title">Country Details</div>
+      <div>
+        <div className="mb-3"><img src={countryService.flagLink(country.alpha2Code)} alt="Country-flag" /></div>
+        <div className="mb-4">{country.name.common}</div> {/* Working with params */}
+        
+        <div className="container ">
+          <div className="row">
+            <div className="col">
+              Capital
+            </div>
+            <div className="col">
+              {country.capital}
+            </div>
+          </div>
+          <div className="row">
+            <div className="col">
+              Area
+            </div>
+            <div className="col">
+              {country.area}
+            </div>
+          </div>
+          <div className="row">
+            <div className="col">
+              Borders:
+            </div>
+            <div className="col">
+              
+              {country.borders.map(borderName => {
+            console.log("Este es el BorderName: " , borderName)
+            return (
 
+                <div key={borderName}>{borderName}</div>
+
+              )}
+              )}
+            </div>
+          </div>
+        </div>
+
+      </div>
+      
       {/*<div>Country Id : {query}</div>  working with query's */}
-
-      <Link to={`/`}>Home</Link>
+      <div>
+        <Link to={`/`}>Home</Link>
+      </div>
     </>
   )
 
